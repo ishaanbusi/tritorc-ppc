c9e520613b480dd63905cc52b514dfec8c444352
ishaanbusi <ishaanbusi@gmail.com>
2025-11-26 15:27:06 +0530

    updates

--- middleware.js
+++ middleware.js
@@ -1,3 +1,4 @@
+export const runtime = "nodejs"; 
 import { NextResponse } from 'next/server';
 import { countries } from './lib/countries';
 
@@ -12,12 +13,14 @@ export function middleware(request) {
 
   if (pathSegments.length >= 2) {
     const [country, lang] = pathSegments;
-
     const countryData = countries[country];
 
     if (countryData && !countryData.languages.includes(lang)) {
       const correctLang = countryData.defaultLang;
-      const newPath = pathname.replace(`/${country}/${lang}`, `/${country}/${correctLang}`);
+      const newPath = pathname.replace(
+        `/${country}/${lang}`,
+        `/${country}/${correctLang}`
+      );
       return NextResponse.redirect(new URL(newPath, request.url));
     }
   }
@@ -26,5 +29,5 @@ export function middleware(request) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)']
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 };
