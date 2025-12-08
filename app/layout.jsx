49ed0323b65578e9c13804ff35b758f658fa4626
ishaanbusi <ishaanbusi@gmail.com>
2025-12-08 13:09:14 +0530

    updates

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* LinkedIn Insight Tag */}
+      <Script id="linkedin-insight" strategy="afterInteractive">
+        {`
+          _linkedin_partner_id = "8987961";
+          window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+          window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+        `}
+      </Script>
+
+      <Script
+        id="linkedin-insight-lib"
+        strategy="afterInteractive"
+        src="https://snap.licdn.com/li.lms-analytics/insight.min.js"
+      />
+
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          alt=""
+          src="https://px.ads.linkedin.com/collect/?pid=8987961&fmt=gif"
+        />
+      </noscript>
+
       <body className={inter.className}>{children}</body>
     </html>
   );
